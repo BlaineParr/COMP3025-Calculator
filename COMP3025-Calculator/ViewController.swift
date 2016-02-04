71b0b00e4b6167ebecd75cebe5ceb7e5c4404815
Blaine Parr <blaineparr@blaines-mac.local>
2016-02-04 17:58:11 -0500

    Initial Commit

---
+++ COMP3025-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COMP3025-Calculator
+//
+//  Created by Blaine Parr on 2016-02-04.
+//  Copyright © 2016 Blaine Parr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
