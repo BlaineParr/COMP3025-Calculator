5f03daff30e5c7e7d1555388c9b648f1d8153ded
Blaine Parr <blaineparr@blaines-mac.local>
2016-02-05 11:50:28 -0500

    Reset is now forced after getting an undefined.

--- COMP3025-Calculator/ViewController.swift
+++ COMP3025-Calculator/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController
     var mathType = -1
     var isFirstNum = true
     var shouldReset = false
+    var forceReset = false
     
     //constants
     let ADD = 0
@@ -74,31 +75,43 @@ class ViewController: UIViewController
     //Button Methods////////////////////////////////////////////////////////////////////////////////////////////////
     @IBAction func zeroButtonClick(sender: UIButton)
     {
-        if(resultsText != "0")
+        if(!forceReset)
         {
-            resultsText += "0"
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
             
-            updateResultsLabel()
+            if(resultsText != "0")
+            {
+                resultsText += "0"
+                
+                updateResultsLabel()
+            } //if ends
         } //if ends
     } //func zeroButtonClick ends
     
     
     @IBAction func oneButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "1"
-        } //if ends
-        else
-        {
-            resultsText += "1"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "1"
+            } //if ends
+            else
+            {
+                resultsText += "1"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func oneButtonClick ends
@@ -106,172 +119,199 @@ class ViewController: UIViewController
     
     @IBAction func twoButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "2"
-        } //if ends
-        else
-        {
-            resultsText += "2"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "2"
+            } //if ends
+            else
+            {
+                resultsText += "2"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func twoButtonClick ends
-
+    
     @IBAction func threeButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "3"
-        } //if ends
-        else
-        {
-            resultsText += "3"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "3"
+            } //if ends
+            else
+            {
+                resultsText += "3"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func threeButtonClick ends
     
     @IBAction func fourButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "4"
-        } //if ends
-        else
-        {
-            resultsText += "4"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "4"
+            } //if ends
+            else
+            {
+                resultsText += "4"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func fourButtonClick ends
     
     @IBAction func fiveButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "5"
-        } //if ends
-        else
-        {
-            resultsText += "5"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "5"
+            } //if ends
+            else
+            {
+                resultsText += "5"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func fiveButtonClick ends
     
     @IBAction func sixButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "6"
-        } //if ends
-        else
-        {
-            resultsText += "6"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "6"
+            } //if ends
+            else
+            {
+                resultsText += "6"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func sixButtonClick ends
     
     @IBAction func sevenButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "7"
-        } //if ends
-        else
-        {
-            resultsText += "7"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "7"
+            } //if ends
+            else
+            {
+                resultsText += "7"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func sevenButtonClick ends
     
     @IBAction func eightButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "8"
-        } //if ends
-        else
-        {
-            resultsText += "8"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "8"
+            } //if ends
+            else
+            {
+                resultsText += "8"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func eightButtonClick ends
     
     @IBAction func nineButtonClick(sender: UIButton)
     {
-        if(shouldReset)
+        if(!forceReset)
         {
-            clearButtonClick(sender)
-            shouldReset = false
-        } //if ends
-        
-        if(resultsText == "0")
-        {
-            resultsText = "9"
-        } //if ends
-        else
-        {
-            resultsText += "9"
-        } //else ends
+            if(shouldReset)
+            {
+                clearButtonClick(sender)
+                shouldReset = false
+            } //if ends
+            
+            if(resultsText == "0")
+            {
+                resultsText = "9"
+            } //if ends
+            else
+            {
+                resultsText += "9"
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func nineButtonClick ends
     
     @IBAction func decimalButtonClick(sender: UIButton)
     {
-        if(resultsText.rangeOfString(".") == nil)
+        if(!forceReset)
         {
-            resultsText += "."
-            
-            updateResultsLabel()
-        } //if ends
+            if(resultsText.rangeOfString(".") == nil)
+            {
+                resultsText += "."
+                
+                updateResultsLabel()
+            } //if ends
+        }
     } //func decimalButtonClick ends
     
     @IBAction func clearButtonClick(sender: UIButton)
@@ -281,20 +321,24 @@ class ViewController: UIViewController
         secondNum = 0
         isFirstNum = true
         posNeg = ""
+        forceReset = false
         
         updateResultsLabel()
     } //func clearButtonClick
     
     @IBAction func posNegButtonClick(sender: UIButton)
     {
-        if(posNeg == "")
+        if(!forceReset)
         {
-            posNeg = "-"
-        } //if ends
-        else
-        {
-            posNeg = ""
-        } //else ends
+            if(posNeg == "")
+            {
+                posNeg = "-"
+            } //if ends
+            else
+            {
+                posNeg = ""
+            } //else ends
+        }
         
         updateResultsLabel()
     } //func posNegButtonClick ends
@@ -302,75 +346,90 @@ class ViewController: UIViewController
     
     @IBAction func percentButtonClick(sender: UIButton)
     {
-        if(!isFirstNum && firstNum != 0)
+        if(!forceReset)
         {
-            stringToNum()
-            
-            isFirstNum = false
-            
-            secondNum /= 100
-            
-            secondNum *= firstNum
-            
-            resultsText = String(secondNum)
-            
-            updateResultsLabel()
-            
-            shouldReset = true
-        } //if ends
-        
-        else
-        {
-            clearButtonClick(sender)
-        } //else ends
+            if(!isFirstNum && firstNum != 0)
+            {
+                stringToNum()
+                
+                isFirstNum = false
+                
+                secondNum /= 100
+                
+                secondNum *= firstNum
+                
+                resultsText = String(secondNum)
+                
+                updateResultsLabel()
+                
+                shouldReset = true
+            } //if ends
+                
+            else
+            {
+                clearButtonClick(sender)
+            } //else ends
+        }
     } //func percentButtonClick ends
     
     @IBAction func divideButtonClick(sender: UIButton)
     {
-        shouldReset = false
-        
-        if(isFirstNum)
+        if(!forceReset)
         {
-            stringToNum()
-        } //if ends
-        
-        mathType = DIV
+            shouldReset = false
+            
+            if(isFirstNum)
+            {
+                stringToNum()
+            } //if ends
+            
+            mathType = DIV
+        }
     } //func divideButtonClick ends
     
     @IBAction func multiplyButtonClick(sender: UIButton)
     {
-        shouldReset = false
-        
-        if(isFirstNum)
+        if(!forceReset)
         {
-            stringToNum()
-        } //if ends
-        
-        mathType = MUL
+            shouldReset = false
+            
+            if(isFirstNum)
+            {
+                stringToNum()
+            } //if ends
+            
+            mathType = MUL
+        }
     } //func multiplyButtonClick ends
     
     @IBAction func subtractButtonClick(sender: UIButton)
     {
-        shouldReset = false
-        
-        if(isFirstNum)
+        if(!forceReset)
         {
-            stringToNum()
-        } //if ends
-        
-        mathType = SUB
+            shouldReset = false
+            
+            if(isFirstNum)
+            {
+                stringToNum()
+            } //if ends
+            
+            mathType = SUB
+        }
     } //subtractButtonClick ends
     
     @IBAction func addButtonClick(sender: UIButton)
     {
-        shouldReset = false
-        
-        if(isFirstNum)
+        if(!forceReset)
         {
-            stringToNum()
-        } //if ends
-        
-        mathType = ADD
+            shouldReset = false
+            
+            if(isFirstNum)
+            {
+                stringToNum()
+            } //if ends
+            
+            mathType = ADD
+        }
     } //addButtonClick ends
     
     @IBAction func calculateButtonClick(sender: UIButton)
@@ -380,7 +439,7 @@ class ViewController: UIViewController
             stringToNum()
             
             var err = false
-        
+            
             switch(mathType)
             {
             case ADD: total = firstNum + secondNum
@@ -412,6 +471,8 @@ class ViewController: UIViewController
             else
             {
                 resultsText = "Undefined"
+                
+                forceReset = true
             } //else ends
             
             shouldReset = true
